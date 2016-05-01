9bb8b2524720770b52e9f60d2ef0250c949ba8af
hspadim <henriquespadim@hotmail.com>
2016-04-30 22:16:01 -0300

    Coloca "this."

--- Pokebola1/src/Pokemon.java
+++ Pokebola1/src/Pokemon.java
@@ -1,4 +1,3 @@
-
 public class Pokemon {
 	String name;
 	int hp;
@@ -6,41 +5,48 @@ public class Pokemon {
 	boolean dead;
 	int i=0;
 	int n_copy=0;
-	public Atack[] atacks=new Atack[4];
-
+	public Atack[] atacks;
+	
 	public Pokemon (String name,int hp){
 		this.name=name;
 		this.hp=hp;
-		dead=false;
-
-		max_hp=hp;
+		this.dead=false;
+		// Coloquei um HP_Max para comparar na funcao GainHp
+		this.max_hp=hp;
+		this.atacks=new Atack[4];
 	}
 
-
+	
 	public void implementAtack(String nome,int atkPri,int q){
-
+				
 		Atack at= new Atack(nome,atkPri,q);
-
-
+		
+		
 		//o indice do ataque é fornecido pela sua prioridade
 
-		atacks[i++]=at;
-
-
+		this.atacks[i++]=at;
+		
+		
 	}
-
-	public Atack getAtack(int n){	
-
-
-		return atacks[n];
+	
+	
+	public Atack getAtack(int n){
+		
+//		atacks[n].evtTime=evtime;
+		if(this.atacks[n]==null)
+			return this.atacks[0];
+		return this.atacks[n];
 	}
 	public boolean isDead(){
-		if(dead==true)
-			return true;
-		else
-			return false;
+		return this.dead;
 	}
 
-
+//	public static int getMax_hp() {
+//		return max_hp;
+//	}
+//
+//	public static void setMax_hp(int max_hp) {
+//		Pokemon.max_hp = max_hp;
+//	}
 
 }
