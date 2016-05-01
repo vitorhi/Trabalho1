7c70d6667096a9125728518aa8101d05b3a3274d
hspadim <henriquespadim@hotmail.com>
2016-04-30 22:16:57 -0300

    Coloque "this."

--- Pokebola1/src/PokemonTrainer.java
+++ Pokebola1/src/PokemonTrainer.java
@@ -2,40 +2,51 @@ import java.util.*;
 public class PokemonTrainer {
 	String name;
 	Queue<Pokemon> queue = new LinkedList<Pokemon>();
-
-
-
-
+	
+	
+	
+	
 	// é aqui que coloquei a prioridade
 	static int _prioridade = 0;
-
+	
 	public PokemonTrainer(Pokemon[]vector_pokemon,String name){
 
 		for(int i=0;i<vector_pokemon.length;i++){
-
+//			System.out.println(vector_pokemon[i].name);
 			queue.add(vector_pokemon[i]);
-
+//			System.out.println(queue.peek().name);
 
 		}
 
 		this.name=name;
-
+		
 		// é aqui que adicionei a prioridade ao jogador
 		_prioridade++;
-
+		
 	}
 	public Pokemon getPokqueue(){
+		
 
-
-		if(queue.peek() == null){
-			System.out.println("Todos os pokemons morreram!");
-			System.out.println("Game Over");
-			Runtime.getRuntime().exit(0);			 			 
-			return null;			 
-		}	 
+		 if(queue.peek() == null){
+			 System.out.println("Todos os pokemons morreram!");
+			 System.out.println("Game Over");
+			 Runtime.getRuntime().exit(0);			 			 
+			 return null;			 
+		 }	 
 		return queue.peek();
 	}
 
+	public Pokemon getSELVAGEMPokqueue(){
+		
+
+		 if(queue.peek() == null){
+			 System.out.println("O pokemon morreu!");
+//			 Runtime.getRuntime().exit(0);			 
+			 return null;			 
+		 }	 
+		return queue.peek();
+	}
+	
 	public SwapPokemon PmonSP(){
 		SwapPokemon SP=new SwapPokemon();
 		return SP;
@@ -45,7 +56,11 @@ public class PokemonTrainer {
 		return UI;
 	}
 	public RunAway PmonRA(){
-		RunAway		RA=new RunAway();
+		RunAway	RA=new RunAway();
 		return RA;
+	}
+	public Morte PmonMorte(){
+		Morte _Morte = new Morte();
+		return _Morte;
 	}
 }
